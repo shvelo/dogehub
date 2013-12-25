0db56a5e35a4bfead6c1abb5a6977ff7268ed922
Stichoza <stichoza@localhost.localdomain>
2013-12-25 19:50:30 +0400

    working socket

---
+++ point.js
@@ -0,0 +1,24 @@
+var app = require('express').createServer(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/cli/dash.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('roger', {
+  	msg: 'roger'
+  });
+
+  socket.on('ohai', function (data) {
+    console.log(
+    	"Random dawg with id " +
+    	data.id +
+    	" has joined :>");
+  });
+
+});
